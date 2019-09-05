11a8ec1ed61325a459bb6b57845aa6c8cdf11fb2
Kyra Mellema <kemellema@gmail.com>
2019-09-05 14:22:58 -0400

    Im committing this

--- leetspeak.py
+++ leetspeak.py
@@ -1,28 +1,30 @@
+# This works but there's no while loop
+# st = ("what doing? do you even lift bro? noob. leet.").upper()
+# st = st.replace("A","4")
+# st = st.replace("E","3")
+# st = st.replace("G","6")
+# st = st.replace("I","1")
+# st = st.replace("O","0")
+# st = st.replace("S","5")
+# st = st.replace("T","7")
+# print(st)
+
+
+#with while loop
+#text = input("What doing? noob. leet.")
+# text = input("What do you want translated? ")
 # alpha = ['A','E','G','I','O','S','T']
 # leet = [4,3,6,1,0,5,7]
-# string = "what doing? do you even lift bro? noob. leet."
+
+# translation = ""
+
+# uppercased_text = text.upper()
+
+
 
 # while len(string) <= 45:
 #     if alfa in string:
 #     print (string.upper())
 
-
 # for alpha in string:
 #     print(leet)
-
-
-
-# #2 lists
-# # each char has while loop
-
-
-# This works but there's no while loop
-st = ("what doing? do you even lift bro? noob. leet.").upper()
-st = st.replace("A","4")
-st = st.replace("E","3")
-st = st.replace("G","6")
-st = st.replace("I","1")
-st = st.replace("O","0")
-st = st.replace("S","5")
-st = st.replace("T","7")
-print(st)
